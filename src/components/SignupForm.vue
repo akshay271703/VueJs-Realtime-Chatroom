2018f39f9eb83a7c28b1dbfc1998072fcf82a32b
akshay_2304 <toakshay.official@gmail.com>
2021-01-03 17:50:26 +0530

    Initial Commit

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+      <input type="text" required placeholder="display name" v-model="display">
+      <input type="email" required placeholder="email" v-model="email">
+      <input type="password" required placeholder="password" v-model="password">
+      <button>Sign Up</button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
